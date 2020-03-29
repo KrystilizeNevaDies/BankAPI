be5fdee13793178a0c668871eff8b45ed02a5f39
Mattes D <github@xoft.cz>
2020-03-29 17:10:34 +0200

    Added API wrappers; general cleanup.
    Converted the functions to use Uuids instead of cPlayer, so that they can be used from console commands.
    Only open the DB once on plugin start, re-use the instance everywhere.
    Redefined the API functions.
    Added API wrappers and instructions on how to use them.

--- API/manage.lua
+++ API/manage.lua
@@ -1,28 +1,55 @@
-
 -- Manage.lua
 
--- Implements functions that can be called by external plugins
+--[[
+Implements functions that can be called by external plugins
+Note that usually external plugins will want to use the ExternalAPI instead (see that file for details)
+--]]
 
 
 
---- Sets the player's selection to the specified cuboid.
--- Returns true on success, false on failure.
-function GetBalance(Player)
-    return GetPlayerBalance(Player)
+
+
+--- Returns the player's current balance
+-- Returns the balance (number) on success, false on failure
+function GetBalance(aPlayerUuid)
+    return GetPlayerBalance(aPlayerUuid)
 end
 
-function SetBalance(Player, Bal)
-    return SetPlayerBalance(Player, Bal)
+
+
+
+
+--- Sets the player's current balance
+-- Returns the player's current balance on success, false on failure
+function SetBalance(aPlayerUuid, aNewBalance)
+    return SetPlayerBalance(aPlayerUuid, aNewBalance)
 end
 
-function AddBalance(Player, Bal)
-    return AddPlayerBalance(Player, Bal)
+
+
+
+
+--- Modifies the player's current balance by the specified amount
+-- Returns the player's new balance on success, false on failure
+function ChangeBalance(aPlayerUuid, aDelta)
+    return ChangePlayerBalance(aPlayerUuid, aDelta)
 end
 
-function RemoveBalance(Player, Bal)
-    return RemovePlayerBalance(Player, Bal)
+
+
+
+
+--- Transfers aAmount from aFromPlayer to aToPlayer, if aFromPlayer has enough funds
+-- Returns true if transfer succeeds, false if not
+function TransferBalance(aFromPlayerUuid, aToPlayerUuid, aAmount)
+    return TransferPlayerBalance(aFromPlayerUuid, aToPlayerUuid, aAmount)
 end
 
-function TransferBalance(aPlayer, bPlayer, Bal)
-    return TransferPlayerBalance(aPlayer, bPlayer, Bal)
+
+
+
+
+--- Returns the location of the ExternalAPI.lua file so that other plugins can load it directly
+function GetExternalAPIPath()
+	return cPluginManager:GetCurrentPlugin():GetLocalFolder() .. "/API/ExternalAPI.lua"
 end
